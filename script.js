e3011e0861ccff1f3ada30433479d338d6c589bc
Abhishek Mishra <abhishekmishra0101010@gmail.com>
2025-04-30 21:06:35 +0530

    Add theme button functionality

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const sunIcon = document.querySelector(".sun");
 const moonIcon = document.querySelector(".moon");
+const root = document.querySelector(":root");
+let darkTheme = document.body.classList.contains("dark");
 
-sunIcon.addEventListener("click", (e) => {
-  sunIcon.classList.add("hidden");
-  moonIcon.classList.remove("hidden");
-});
+if (localStorage.getItem("theme") === "dark") {
+  themeChanger("white", `#111111`);
+}
 
 moonIcon.addEventListener("click", (e) => {
-  moonIcon.classList.add("hidden");
-  sunIcon.classList.remove("hidden");
+  themeChanger("white", `#111111`);
+  localStorage.setItem("theme", "dark");
 });
+
+sunIcon.addEventListener("click", (e) => {
+  themeChanger(`#000`, "#e7e3d3");
+  localStorage.setItem("theme", "");
+});
+
+function themeChanger(text, background) {
+  sunIcon.classList.toggle("hidden");
+  moonIcon.classList.toggle("hidden");
+  document.body.classList.toggle("dark");
+  root.style.setProperty("--background", background);
+  root.style.setProperty("--text", text);
+  darkTheme = document.body.classList.contains("dark");
+}
