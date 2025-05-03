f09646371c7a42120dc4f9d19b4c7bca2a959cf1
Abhishek Mishra <abhishekmishra0101010@gmail.com>
2025-05-03 12:45:21 +0530

    Add popup on about btn click

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const sunIcon = document.querySelector(".sun");
 const moonIcon = document.querySelector(".moon");
+const aboutBtn = document.querySelector("#about-btn");
+const closeIcon = document.querySelector(".close-icon");
+const aboutContainer = document.querySelector(".about-container");
+const aboutPopup = document.querySelector(".about-popup");
 const root = document.querySelector(":root");
 let darkTheme = document.body.classList.contains("dark");
 
@@ -15,6 +19,22 @@ moonIcon.addEventListener("click", (e) => {
 sunIcon.addEventListener("click", (e) => {
   themeChanger(`#000`, "#e7e3d3");
   localStorage.setItem("theme", "");
+});
+
+aboutBtn.addEventListener("click", (e) => {
+  aboutContainer.classList.toggle("hidden");
+});
+
+closeIcon.addEventListener("click", (e) => {
+  aboutContainer.classList.toggle("hidden");
+});
+
+aboutContainer.addEventListener("click", (e) => {
+  aboutContainer.classList.toggle("hidden");
+});
+
+aboutPopup.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
 
 function themeChanger(text, background) {
